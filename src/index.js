95ed560d757c1dd7444c3504a0e0cb02eb5a0927
antonio <ajti_1989@hotmail.com>
2020-04-18 16:13:25 +0200

    implementacion de nodemon

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-
-//Capturar errores, hay mas acciones
-process.on('unhandledRejection', (err,p) =>{
-    console.log(`Custom unhandledRejection ${err}`)
-})
-
-
-process.on('uncaughtException', (err) => {
-    console.log(`Custom uncaughtException ${err}`)
-})
-// Promise(resolve => JSON.parse({color: 'blue'}))
-
-throw 'mi error'
+console.log("hola mundo")
+console.log("y ahora que")
