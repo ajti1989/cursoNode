c1044d16e0eec13f6177eafa6bd9b77de462665a
antonio <ajti_1989@hotmail.com>
2020-04-18 11:12:54 +0200

    dependencia babel y ejemplo de su uso

---
+++ compilado.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var value = 24;
+
+var miFn = function miFn(num) {
+  return value * 10;
+};
+
+console.log(miFn(value));
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const value = 24
+
+const miFn = num => value * 10
+
+console.log(miFn(value))
