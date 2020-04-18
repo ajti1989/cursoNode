ea9016b9f2052ca6eae9805303b49c321719a10e
antonio <ajti_1989@hotmail.com>
2020-04-18 16:31:26 +0200

    puesta en marcha de un servidor simple y bloqueando peticiones GET

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-console.log("hola mundo")
-console.log("y ahora que")
+import http, { request } from 'http'
+
+const server = http.createServer((request, response) => {
+    if(request.method === 'GET'){
+        response.write('<h1>Metodo no permitido</h1>')
+        response.end()
+    }
+    response.write('<h1>estás accediendo a un servidor</h1>')
+        response.end()
+})
+
+server.listen(8000, 'localhost', err => {
+    if (err){
+        return console.log('Error: ', err)
+    }
+
+    console.log('Server abierto en http://LOCALHOST:8000')
+})
