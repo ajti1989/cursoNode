4497fa5192bdacf37800f8f0fab9e65a18714590
antonio <ajti_1989@hotmail.com>
2020-04-18 20:00:40 +0200

    creación vista con pug (motor de plantillas)

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
 import path from 'path'
 import express from 'express'
-import morgan from 'morgan'
+import logger from 'morgan'
 import bodyParser from 'body-parser'
 
 const app = express()
+
+//desactiva el "avisador" de que está hecho con express
 app.disable('x-powered-by')
 
+//define que está en estado de desarrollo
 app.set('env', 'development')
 
-//middleware morgan
-app.use(morgan('tiny'))
+//middleware morgan (se pueden configurar como muestra los log)
+app.use(logger('tiny'))
 
 //middleware body-parser
 //parsea a json todo los que llega por una petición post
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+//vistas
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+//enrutado
 app.get('/', (req, res) =>{
-    res.end('Hola mundo')
+    res.render('home', {
+       title: 'Curso NodeJS',
+       message: 'Estoy realizando el curso' 
+    })
 })
 
+//server lanzado en el puerto 9000
 app.listen('9000', () => {
     console.log('Servidor en marcha (Port:9000)')
 })
