dcf076098692b56d31a07510554964f2a0113284
antonio <ajti_1989@hotmail.com>
2020-04-18 13:40:39 +0200

    mismo algoritmo hecho de forma asincrona y con eventos

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-import fs from 'fs'
+import readline from 'readline'
+
+import async from './async'
+import events from './events'
 
 const file = process.argv[2]
 
-//al no ser asincrono hasta que no termina no se muestra, por esa razón se ejecuta antes la última linea de código
-fs.readFile(file, (err, contents) =>{
-    if(err){
-        return console.log(err)
-    }
-
-    const lines = contents.toString().split('\n')
-
-    for(let line of lines){
-        console.log(`Número de caracteres por linea: ${line.length}`)
-    }
-
-    console.log(`Número de elementos que : ${lines.length}`)
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 })
 
-console.log(`Se está leyendo el fichero: ${file}`)
+rl.question(
+    `Como quiere leer el fichero?
+    1. De forma asincrona (default)
+    2. Con eventos
+    Seleccione una opcion: `,
+    value => {
+        console.log(`Ha seleccionado el valor ${value}\n\n`)
+
+        switch (value){
+            case '2':
+                events(file)
+                break;
+            case '1':
+                default:
+                    async(file)
+        }
+
+        rl.close()
+    }
+
+
+)
